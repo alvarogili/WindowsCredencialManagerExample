9750a7175cb9e6bd749ff5fb5530655119483c53
alvarogili <tarde271011>
2020-11-27 08:53:38 -0300

    Update readme

--- WindowsCredencialManagerExample/Program.cs
+++ WindowsCredencialManagerExample/Program.cs
@@ -7,7 +7,7 @@ namespace WindowsCredencialManagerExample
     {
         public static void Main(string[] args)
         {
-            //https://stackoverflow.com/questions/17741424/retrieve-credentials-from-windows-credentials-store-using-c-sharp
+            //https://stackoverflow.com/questions/17741424/retrieve-credentials-from-windows-credentials-store-using-c-sharp/17747020#17747020
             CredentialUtil.SetCredentials("FOO", "john", "1234", PersistanceType.LocalComputer);
             var userpass = CredentialUtil.GetCredential("FOO");
             Console.WriteLine($"User: {userpass.Username} Password: {userpass.Password}");
--- WindowsCredencialManagerExample/README.md
+++ WindowsCredencialManagerExample/README.md
@@ -1,3 +1,5 @@
 ﻿# Example for Retrieve credentials from Windows Credentials Store using C#
 
-This is a example that access to Windows Credential Manager, creates a key and gets it.
+This is a example that access to Windows Credential Manager(https://support.microsoft.com/en-us/windows/accessing-credential-manager-1b5c916a-6a16-889f-8581-fc16e8165ac0), creates a key and gets it.
+
+Based in: https://stackoverflow.com/questions/17741424/retrieve-credentials-from-windows-credentials-store-using-c-sharp/17747020#17747020
